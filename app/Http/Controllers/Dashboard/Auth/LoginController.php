7dfc41c209ce74cde56274f4ccc4fff6af17182f
mhkhaldi95 <35767463+mhkhaldi95@users.noreply.github.com>
2022-12-28 20:53:39 +0200

    first

---
+++ app/Http/Controllers/Dashboard/Auth/LoginController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Dashboard\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Dashboard\LoginRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
+
+class LoginController extends Controller
+{
+    public function index(){
+        return view('dashboard/auth/signin');
+    }
+    public function login(LoginRequest $request)
+    {
+        $credentials = $request->only('email', 'password');
+        if (Auth::attempt($credentials)) {
+            return redirect()->route('dashboard.index')->with([
+                'success' => 'Signed in'
+            ]);
+        }
+        return redirect()->route('login')->with([
+            'error' => 'Login details are not valid'
+        ]);
+    }
+    public function logout(){
+        Session::flush();
+        auth('web')->logout();
+
+        return redirect()->route('login')->with([
+            'success' => 'Signed out Successfully'
+        ]);
+    }
+}
