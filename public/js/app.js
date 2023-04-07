313b4af3819ebe1e8ff21dcfd6a7c62da7992c66
mhkhaldi95 <35767463+mhkhaldi95@users.noreply.github.com>
2023-04-07 21:24:18 +0300

    .

--- public/js/app.js
+++ public/js/app.js
@@ -19901,22 +19901,13 @@ function startFCM() {
 }
 var xInterval = null;
 (0,firebase_messaging__WEBPACK_IMPORTED_MODULE_3__.onMessage)(messaging, function (payload) {
-  console.log("payload.data.type == 'branch_accept_order'", payload.data.type == 'branch_accept_order');
-  console.log("payload.data.type == 'branch_accept_order'", payload.data.type + ' | branch_accept_order');
   if (payload.data.type == 'new_order') {
     countDown(payload.data);
   } else if (payload.data.type == 'branch_accept_order') {
-    // $('#countDownWebsite').modal('hide')
-    // console.log("payload.data.order_id",payload.data.order_id)
-    // var x = localStorage.getItem('xInterval'+payload.data.order_id)
-    // clearInterval(x);
-    // Get a reference to the last interval + 1
-    var interval_id = window.setInterval(function () {}, Number.MAX_SAFE_INTEGER);
-
-    // Clear any timeout/interval up to that id
-    for (var i = 1; i < interval_id; i++) {
-      window.clearInterval(i);
-    }
+    $('#countDownWebsite').modal('hide');
+    console.log("payload.data.order_id", payload.data.order_id);
+    var x = localStorage.getItem('xInterval' + payload.data.order_id);
+    clearInterval(x);
     toastr.success('تم قبول الطلبية بنجاح');
   } else if (payload.data.type == 'branch_reject_order') {
     $('#countDownWebsite').modal('hide');
